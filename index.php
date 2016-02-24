8fb85679e34b6f62ca7422bf97218db6bf6849d2
Jason Katz <jkatz94@gmail.com>
2016-02-24 12:07:27 -0500

    linked semantic ui

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <html>
     <head>
         <title>NETS</title>
+
+
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.1.8/semantic.min.css">
     </head>
     <body>
         <h1>hey there</h1>
